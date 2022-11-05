2bd98e3fc92c8dc6d59226be121dd1059737dc38
Yann Darwish <yann.darwish@gmail.com>
2022-11-05 18:09:28 +0100

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router/router';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider } from "react-router-dom"
+import router from "./router/router"
+import { store } from "./redux/store"
+import { Provider } from "react-redux"
 
-import './App.css'
-import './components/Header/Header.css'
-import './components/Hero/Hero.css'
-import './components/Feature-Item/Feature-Item.css'
-import './components/Sign-In-Form/SignInForm.css'
-import './pages/User/User.css'
-import './components/Account/Account.css'
-import './components/Footer/Footer.css'
+import "./App.css"
+import "./components/Header/Header.css"
+import "./components/Hero/Hero.css"
+import "./components/Feature-Item/Feature-Item.css"
+import "./components/Sign-In-Form/SignInForm.css"
+import "./pages/User/User.css"
+import "./components/Account/Account.css"
+import "./components/Footer/Footer.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <RouterProvider router={router} />
-);
-
+	<Provider store={store}>
+		<RouterProvider router={router} />
+	</Provider>
+)
